c0ad4eadad2e4f34241b82f8bac82678b6a0c7dc
Edwin <Edwinallen94@aol.com>
2023-06-13 13:09:10 -0400

    class work, 03 props

--- exercises/03-props/src/App.jsx
+++ exercises/03-props/src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
 // import something here
+import Welcome from "./components/Welcome";
+import Heading from "./components/Heading";
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return (
+    <div className="App">
+      <Welcome name="Jamal" />
+      <Welcome />
+      <Heading>Welcome to my React App!</Heading>
+    </div>
+  );
 }
 
 export default App;
