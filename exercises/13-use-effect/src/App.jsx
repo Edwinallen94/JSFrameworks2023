c17aa0670bb3aca08b9da740958f583335e94ee2
Edwin <Edwinallen94@aol.com>
2023-07-24 17:26:51 -0700

    commited changes

--- exercises/13-use-effect/src/App.jsx
+++ exercises/13-use-effect/src/App.jsx
@@ -1,21 +1,44 @@
 // import useEffect
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 // import Axios (or use Fetch)
 
 function App() {
   /**
    * Set up state and make AJAX requests here
+   *
+   *
    */
+
+const [characters, setCharacters] =useState([]);
+
+const [image, setImage] = useState("");
+const [name, setName] = useState("");
+
+useEffect(() => {
+  axios("https://rickandmortyapi.com/api/character/").then((resp) => {
+    setCharacters(resp.data.results);
+  });
+}, []);
+
+const getCharacter = (id => {
+  axios(`https://rickandmortyapi.com/api/character/${id}`).then((resp) => {
+    setImage(resp.data.image);
+    setName(resp.data.name);
+  });
+});
+};
 
   return (
     <div className="container">
       <div className="row text-center" id="body">
-        <h1 id="title-head">{/* Plugin character name here */}</h1>
+        <h1 id="title-head">{name}</h1>
         <div id="main-img">
           <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
             {/* Add an alt and src to this image */}
-            <img height="250" />
+
+            <img  height="250" />
           </a>
           <div className="linkfooter">
             <p>Select your favorite character</p>
